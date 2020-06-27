6d208e79a029d77561aff77f251ec2f46e989380
moortality <67442250+moortality@users.noreply.github.com>
2020-06-27 14:24:13 +0300

    Add files via upload

---
+++ passgen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+valchars='^[1-9]+$'
+
+if ! [[ $1 =~ $valchars ]] || [[ $# -eq 0 ]] ; then
+    echo 'usage: '$0' number-of-characters'
+    exit 0
+fi
+
+< /dev/urandom tr -dc "_$'@'#%{}[]()/\',;:.<>~\'"'"'"'"'`'A-Z-a-z-0-9 | head -c${1:-$1};echo;
